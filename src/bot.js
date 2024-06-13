81975a633b42cd39bb54b9c1dbd04e1c919a971d
dprostiruk <67105855+dprostiruk@users.noreply.github.com>
2024-06-13 14:47:45 +0300

    Init

---
+++ src/bot.js
@@ -0,0 +1,20 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '7393065097:AAE7O9-9w4Sn9tVTttW5dwqdBrzlu6LoUR8';
+const webAppUrl = 'https://google.com';
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text === '/start') {
+    await bot.sendMessage(chatId, `Welcome to the bot!`, {
+        reply_markup: {
+            inline_keyboard: [
+            [{text: 'Open Web App', url: webAppUrl}]
+            ]
+        }
+    });
+  }
+});
