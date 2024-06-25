1950bb1d8978b797eff3d5c1990fda526353be8f
dprostiruk <67105855+dprostiruk@users.noreply.github.com>
2024-06-26 00:47:41 +0300

    test

--- src/index.js
+++ src/index.js
@@ -4,46 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Before you start, make sure you have installed the @grafana/faro-web-sdk (>=v1.4.0) with your package manager of choice
-// Add the following code snippet to your application before any other JavaScript/TypeScript code!
-// For example put the code in your root index.[ts|js] file, right before you initialize your SPA / App.
-
-import { getWebInstrumentations, initializeFaro, ReactIntegration, ReactRouterVersion } from '@grafana/faro-react';
-import { TracingInstrumentation } from '@grafana/faro-web-tracing';
-
-import { createRoutesFromChildren, matchRoutes, Routes, useLocation, useNavigationType } from 'react-router-dom';
-
-initializeFaro({
-  url: 'https://faro-collector-prod-eu-north-0.grafana.net/collect/6de245704282902ceae50a44ed6d2a7e',
-  app: {
-    name: 'test',
-    version: '1.0.0',
-    environment: 'production'
-  },
-  
-  instrumentations: [
-    // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
-    ...getWebInstrumentations(),
-
-    // Initialization of the tracing package.
-    // This packages is optional because it increases the bundle size noticeably. Only add it if you want tracing data.
-    new TracingInstrumentation(),
-
-    new ReactIntegration({
-      router: {
-        version: ReactRouterVersion.V6,
-        dependencies: {
-          createRoutesFromChildren,
-          matchRoutes,
-          Routes,
-          useLocation,
-          useNavigationType,
-        },
-      },
-    }),
-  ],
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
